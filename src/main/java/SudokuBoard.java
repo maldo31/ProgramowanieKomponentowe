9ffdf747bb82878b7bf24ae748c184b4a0baefc8
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-10-25 22:00:34 +0100

    bugfix

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -41,7 +41,7 @@ public class SudokuBoard {
             }
         }
        for (int i = 0; i<3;i++){
-           for(int j=0;i<3;j++){
+           for(int j=0;j<3;j++){
                if(active == board[subrow + i][subcol + j]){
                    if(subrow+i!=row){
                        if(subcol+j!=column){
