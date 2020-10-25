006f6ca5a9e3cee18350f2c7c6169b59f1fedcbb
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-10-25 19:09:42 +0100

    Added checking if number is right in row and column

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -3,26 +3,44 @@ import java.util.Random;
 public class SudokuBoard {
     private int[][] board = new int[9][9];
 
-    public int[][] getCopyOfBoard(){
+    public int[][] getCopyOfBoard() {
         int[][] copiedBoard = new int[9][9];
-        for (int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 copiedBoard[i][j] = board[i][j];
             }
         }
         return copiedBoard;
     }
-    
-    public void showBoard(){
-        for(int i=0; i< 9; i++){
-            for(int j=0; j<9; j++){
-                System.out.print(board[i][j]+ " ");
+
+    public void showBoard() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.print("\n");
         }
     }
 
-}
+    private boolean checkCell(int row, int column) {
+        int active = board[row][column];
+        for (int i = 0; i < 9; i++) {
+            if (active == board[row][i]) {
+                if (column != i) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (active == board[column][i]) {
+                if (row != i) {
+                    return false;
+                }
+            }
+        }
 
+        return true;
+    }
+}
 
 
