c2652f22b5ab57e2cb6a73d85f5a6b35ae74d00b
230039 Włodarczyk Maciej <230039@edu.p.lodz.pl>
2020-10-25 18:28:07 +0100

    Added showBoard class

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class SudokuBoard {
     int[][] board = new int[9][9];
-    public static void fillBoard(
 
-    )
 }
+
+public void showBoard(){
+    for(int i=0; i< 9; i++){
+        for(int j=0; j<9; j++){
+            System.out.print(board[i][j]+ " ");
+        }
+        System.out.print("\n");
+    }
+}}
