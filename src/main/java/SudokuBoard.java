b26a2272880f49c20f82adbe36b6cad4e1544213
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-10-25 18:32:41 +0100

    Bug fix

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -2,14 +2,14 @@ import java.util.Random;
 
 public class SudokuBoard {
     int[][] board = new int[9][9];
-
-}
-
-public void showBoard(){
-    for(int i=0; i< 9; i++){
-        for(int j=0; j<9; j++){
-            System.out.print(board[i][j]+ " ");
+    public void showBoard(){
+        for(int i=0; i< 9; i++){
+            for(int j=0; j<9; j++){
+                System.out.print(board[i][j]+ " ");
+            }
+            System.out.print("\n");
         }
-        System.out.print("\n");
-    }
-}}
+    }}
+
+
+
