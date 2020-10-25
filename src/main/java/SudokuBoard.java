24129c83327ae38790b1299f4a2edb98314758cf
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-10-25 19:35:35 +0100

    bugfix

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -27,7 +27,7 @@ public class SudokuBoard {
     private boolean checkCell(int row, int column) {
         int active = board[row][column];
         int subrow = row - row % 3;
-        int subcol = col - col % 3;
+        int subcol = column - column % 3;
         for (int i = 0; i < 9; i++) {
             if (active == board[row][i]) {
                 if (column != i) {
@@ -43,8 +43,8 @@ public class SudokuBoard {
             }
         }
        for (int i = 0; i<3;i++){
-           for(int j=;i<3;j++){
-               if(active=board[subrow+i][subcol+j]){
+           for(int j=0;i<3;j++){
+               if(active == board[subrow + i][subcol + j]){
                    if(subrow+i!=row){
                        if(subcol+j!=column){
                            return false;
