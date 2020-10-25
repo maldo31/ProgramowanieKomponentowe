f8d36b7696f1f99e957852a6006f912e175effec
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-10-25 23:27:08 +0100

    backtracking implementation

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -34,7 +34,7 @@ public class SudokuBoard {
             }
         }
         for (int i = 0; i < 9; i++) {
-            if (active == board[column][i]) {
+            if (active == board[i][column]) {
                 if (row != i) {
                     return false;
                 }
@@ -59,15 +59,55 @@ public class SudokuBoard {
         boolean flag;
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                if(board[i][j]==0){
+                //if(board[i][j]==0){
                     flag = checkCell(i, j);
-                    if (flag == false){
-                        board[i][j] = random.nextInt(9);
+                    int k=0;
+                    int randomDigit = 1+random.nextInt(9);
+                    int[] digits = new int[10];
+                    int s=0;
+                    while(randomDigit<10){
+
+                        digits[s]=randomDigit;
+
+                        randomDigit=randomDigit+1;
+                        s++;
+
+                    }
+                    int l=0;
+                    while(s<9){
+                        digits[s]=1+l;
+
+                        l++;
+                        s++;
+                    }
+                    System.out.print("||||||||||||||||||||||||||||\n");
+                    do{
+                        board[i][j] = digits[k];
+                        System.out.print("-------------"+k+"----------\n");
+                        showBoard();
+                        k++;
+                        ;
+                    }
+                    while(checkCell(i,j) ==false && k<10);
+                    if (k==10){
+                        if(j>0){
+                            j=j-2;
+                        }
+                        else {
+
+                            if(i>1){
+                                i=i-1;
+                            }
+
+                        }
+                    }
+
+
                     }
                 }
             }
-        }
-    }
+        //}
+
 
     public static void main(String[] args) {
         showBoard();
