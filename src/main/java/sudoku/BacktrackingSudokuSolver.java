68452976e575c17871c802faa3a8100609b088e3
230039 Włodarczyk Maciej <230039@edu.p.lodz.pl>
2020-11-01 20:06:45 +0100

    Changed size of board from digit to a variable

--- src/main/java/sudoku/BacktrackingSudokuSolver.java
+++ src/main/java/sudoku/BacktrackingSudokuSolver.java
@@ -6,43 +6,39 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
 
     public void solve(SudokuBoard board) {
         {
-            int[] randomDigits = new int[81];
+            int size = 9;
+            int[] randomDigits = new int[size * size];
 
             Random random = new Random();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
                     boolean valid = false;
-                    if (randomDigits[i * 9 + j] == 0) {
-                        randomDigits[i * 9 + j] = 1 + random.nextInt(9);
-                        board.set(i,j,randomDigits[i * 9 + j]);
-
+                    if (randomDigits[i * size + j] == 0) {
+                        randomDigits[i * size + j] = 1 + random.nextInt(size);
+                        board.set(i,j,randomDigits[i * size + j]);
                         do {
                             if (board.checkCell(i, j) == true) {
                                 valid = true;
                                 break;
                             }
-
-                            board.set(i,j,board.get(i,j) % 9 + 1);
-                        } while (randomDigits[i * 9 + j] != board.get(i,j));
+                            board.set(i,j,board.get(i,j) % size + 1);
+                        } while (randomDigits[i * size + j] != board.get(i,j));
                     } else {
-                        board.set(i,j,board.get(i,j) % 9 + 1);
-                        while (board.get(i,j) != randomDigits[i * 9 + j]) {
+                        board.set(i,j,board.get(i,j) % size + 1);
+                        while (board.get(i,j) != randomDigits[i * size + j]) {
                             if (board.checkCell(i, j) == true) {
                                 valid = true;
                                 break;
                             }
-                            board.set(i,j,board.get(i,j) % 9 + 1);
-
+                            board.set(i,j,board.get(i,j) % size + 1);
                         }
                     }
                     if (!valid) {
                         board.set(i,j,0);
-                        randomDigits[i * 9 + j] = 0;
+                        randomDigits[i * size + j] = 0;
                         if (j > 0) {
-
                             j = j - 2;
                         } else if (j == 0) {
-
                             i = i - 1;
                             j = 7;
                         }
@@ -50,6 +46,5 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
                 }
             }
         }
-
     }
 }
--- src/main/java/sudoku/SudokuBoard.java
+++ src/main/java/sudoku/SudokuBoard.java
@@ -1,7 +1,5 @@
 package sudoku;
 
-
-
 /*
     //Sprawdzanie czy wtyczka checkstyle działa
     //Po odkomentowaniu checkstyle informuje o braku spacji między znakami
@@ -11,45 +9,47 @@ package sudoku;
 */
 
 public class SudokuBoard {
-    private final int[][] board = new int[9][9];
+    int size = 9;
+    private final int[][] board = new int[size][size];
 
     public void solveGame() {
         SudokuSolver solver = new BacktrackingSudokuSolver();
         solver.solve(this);
     }
 
-
     public int[][] getCopyOfBoard() {
         int[][] copiedBoard = new int[9][9];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 copiedBoard[i][j] = board[i][j];
             }
         }
         return copiedBoard;
     }
 
+    /*
     public void showBoard() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 System.out.print(get(i,j) + " ");
             }
             System.out.print("\n");
         }
     }
+     */
 
     public boolean checkCell(int row, int column) {
         int active = board[row][column];
         int subrow = row - row % 3;
         int subcol = column - column % 3;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < size; i++) {
             if (active == board[row][i]) {
                 if (column != i) {
                     return false;
                 }
             }
         }
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < size; i++) {
             if (active == board[i][column]) {
                 if (row != i) {
                     return false;
@@ -77,9 +77,6 @@ public class SudokuBoard {
     public void set(int row, int column, int value) {
         this.board[row][column] = value;
     }
-
-
-
 
 }
 
