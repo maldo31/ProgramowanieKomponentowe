a7f3b67c5c4e938a9d8cff6b26bf7e70716922bc
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-11-07 17:37:43 +0100

    added comments

--- src/main/java/sudoku/BacktrackingSudokuSolver.java
+++ src/main/java/sudoku/BacktrackingSudokuSolver.java
@@ -3,16 +3,11 @@ import java.util.*;
 
 public class BacktrackingSudokuSolver implements SudokuSolver {
     private static final int UNASSIGNED = 0;
+    /*Algorytm backtracking zaczerpniety ze strony https://www.geeksforgeeks.org/sudoku-backtracking-7/
+    (jednakże jest to jego powszechna wersja, i wiele innych źródeł implementuje algorytm w ten sam sposób),
+    został odpowiednio zmodyfikowany do zaleceń z zadania.
+     */
     public boolean solve(SudokuBoard board,int num) {
-        /*if(board.get(board.size-1,board.size-1)!=0) {
-            for (int row = 0; row < 9; row++) {
-                    for (int col = 0; col < 9; col++) {
-                        board.set(row, col, 0);
-                    }
-                }
-            }
-
-         */
         Random random = new Random();
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
@@ -43,15 +38,7 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         return true;
     }
     public boolean solve(SudokuBoard board) {
-        /*if(board.get(board.size-1,board.size-1)!=0) {
-            for (int row = 0; row < 9; row++) {
-                    for (int col = 0; col < 9; col++) {
-                        board.set(row, col, 0);
-                    }
-                }
-            }
 
-         */
         Random random = new Random();
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
@@ -87,46 +74,3 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         return true;
     }
 }
-        /*
-            int size = board.size;
-            int[] randomDigits = new int[size * size];
-
-            Random random = new Random();
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-                    boolean valid = false;
-                    if (randomDigits[i * size + j] == 0) {
-                        randomDigits[i * size + j] = 1 + random.nextInt(size);
-                        board.set(i,j,randomDigits[i * size + j]);
-                        do {
-                            if (board.checkCell(i, j) == true) {
-                                valid = true;
-                                break;
-                            }
-                            board.set(i,j,board.get(i,j) % size + 1);
-                        } while (randomDigits[i * size + j] != board.get(i,j));
-                    } else {
-                        board.set(i,j,board.get(i,j) % size + 1);
-                        while (board.get(i,j) != randomDigits[i * size + j]) {
-                            if (board.checkCell(i, j) == true) {
-                                valid = true;
-                                break;
-                            }
-                            board.set(i,j,board.get(i,j) % size + 1);
-                        }
-                    }
-                    if (!valid) {
-                        board.set(i,j,0);
-                        randomDigits[i * size + j] = 0;
-                        if (j > 0) {
-                            j = j - 2;
-                        } else if (j == 0) {
-                            i = i - 1;
-                            j = 7;
-                        }
-                    }
-                }
-            }
-        }   */
-
-
