d253cbf5b2b162cd15d5d0a412d305a80599c871
230039 Włodarczyk Maciej <230039@edu.p.lodz.pl>
2020-12-05 16:01:42 +0100

    Added implementation. It's not working - probably needed to use IOException

--- src/main/java/sudoku/FileSudokuBoardDao.java
+++ src/main/java/sudoku/FileSudokuBoardDao.java
@@ -2,6 +2,8 @@ package sudoku;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class FileSudokuBoardDao implements Dao<SudokuBoard> {
 
@@ -14,14 +16,21 @@ private String fileName;
     @Override
     public void write(SudokuBoard object) {
         FileOutputStream fos = new FileOutputStream(fileName);
-
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(object);
     }
 
     @Override
     public SudokuBoard read() {
+        SudokuBoard object = null;
 
+        FileInputStream fis = new FileInputStream(fileName);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        object = (SudokuBoard) ois.readObject();
+
+        return object;
     }
-    
+
 
     // Finalize nie może być użyte, bo finalize istnieje już w java.lang.Object
     // i jest protected <???>
