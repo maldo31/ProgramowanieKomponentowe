d0d85faae4163c252d6bdf3e772eaa99ad43d068
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-12-19 19:47:13 +0100

    Added start game window

--- javafx/src/main/java/org/comp/PrimaryController.java
+++ javafx/src/main/java/org/comp/PrimaryController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void switchToBoard() throws IOException {
+        App.setRoot("board");
     }
 }
