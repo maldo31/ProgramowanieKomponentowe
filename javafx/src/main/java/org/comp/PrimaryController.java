45858439a622d3515a1ac30c0d546aa9e10eef35
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2020-12-19 20:22:09 +0100

    Work on choice board

--- javafx/src/main/java/org/comp/PrimaryController.java
+++ javafx/src/main/java/org/comp/PrimaryController.java
@@ -1,12 +1,25 @@
 package org.comp;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    private static String level;
+
     @FXML
     private void switchToBoard() throws IOException {
         App.setRoot("board");
+    }
+    @FXML
+    public void onActionButtonStartGame(ActionEvent actionEvent) throws IOException {
+        if (!level == null) {
+            FxmlStageSetup.buildStage("boardWindow.fxml");
+        } else {
+            popOutWindow.messageBox("Warning",
+                    "Check if language and level has not been chosen", Alert.AlertType.WARNING);
+        }
     }
 }
