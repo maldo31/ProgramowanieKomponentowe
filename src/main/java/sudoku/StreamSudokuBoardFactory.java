44ffd91962bcb808d094c34c68215f682b7d9e0c
230039 Włodarczyk Maciej <230039@edu.p.lodz.pl>
2020-12-19 21:14:34 +0100

    Deleted old project files

--- src/main/java/sudoku/StreamSudokuBoardFactory.java
+++
@@ -1,7 +0,0 @@
-package sudoku;
-
-public class StreamSudokuBoardFactory extends SudokuBoardDaoFactory {
-    public Dao getFileDao(String fileName) {
-        return new FileSudokuBoardDao(fileName);
-    }
-}
