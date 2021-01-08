d6a1c79264045dfc7bbe2b363d693bb2b7166215
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-01-08 01:32:01 +0100

    Save&Load features works ok

---
+++ prawidlowyFX/src/main/java/sudoku/view/LoadedController.java
@@ -0,0 +1,115 @@
+package sudoku.view;
+
+
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import javafx.util.converter.NumberStringConverter;
+import sudoku.model.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LoadedController implements Initializable {
+    private Stage thisStage;
+
+    @FXML
+
+    private GridPane sudokuBoardGrid;
+    private SudokuBoard sudokuBoard;
+
+    private PopOutWindow popOutWindow = new PopOutWindow();
+    private BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+    private Level difficultyLevel = new Level();
+
+
+    private static String language = App.getLanguage();
+
+
+    public TextArea textArea;
+    private FileChooser fileChooser;
+    private File file;
+
+    public LoadedController(SudokuBoard sudokuBoard) {
+
+        thisStage = new Stage();
+        this.sudokuBoard=sudokuBoard;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("secondary.fxml"));
+            Locale.setDefault(new Locale(language));
+            ResourceBundle bundle = ResourceBundle.getBundle("bundles.languages");
+            loader.setResources(bundle);
+            loader.setController(this);
+
+
+            thisStage.setScene(new Scene(loader.load()));
+            thisStage.setTitle(bundle.getString("application_title"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void fillGrid() {
+        StringConverter<Number> converter = new NumberStringConverter();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                TextField textField = new TextField();
+                textField.setMinSize(50, 50);
+                textField.setFont(Font.font(18));
+                Bindings.bindBidirectional(textField.textProperty(), sudokuBoard.getProperty(i,j),converter);
+                if (sudokuBoard.get(i, j) != 0) {
+                    textField.setDisable(true);
+                    textField.setText(String.valueOf(sudokuBoard.get(i, j)));
+                }
+                sudokuBoardGrid.add(textField, i, j);
+
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        fillGrid();
+        textArea.appendText("Sprawdz Uklad");
+
+    }
+    public void showStage() {
+        thisStage.showAndWait();
+    }
+
+    @FXML
+    public void onActionButtonCheck(ActionEvent actionEvent) throws IOException {
+
+        if(sudokuBoard.checkBoard()==true) {
+            textArea.clear();
+            textArea.appendText("Układ Prawidłowy");
+        }
+        else {
+            textArea.clear();
+            textArea.appendText("Układ Nieprawidłowy");
+        }
+    }
+    @FXML
+    public void onActionButtonSaveGame(ActionEvent actionEvent)  {
+        fileChooser = new FileChooser();
+        file = fileChooser.showSaveDialog(thisStage);
+        SudokuBoardDaoFactory factory = new StreamSudokuBoardFactory();
+        Dao<SudokuBoard> sudokuBoardDaoFile;
+        sudokuBoardDaoFile = factory.getFileDao(file.getAbsolutePath());
+        sudokuBoardDaoFile.write(sudokuBoard);
+
+    }
+}
--- prawidlowyFX/src/main/java/sudoku/view/PrimaryController.java
+++ prawidlowyFX/src/main/java/sudoku/view/PrimaryController.java
@@ -1,18 +1,30 @@
 package sudoku.view;
-import java.io.IOException;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import sudoku.model.Dao;
+import sudoku.model.StreamSudokuBoardFactory;
+import sudoku.model.SudokuBoard;
+import sudoku.model.SudokuBoardDaoFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class PrimaryController {
+
+    private Stage thisStage;
 
     private static String level;
     @FXML
     private ComboBox comboBoxSystemDifficult;
+    private FileChooser fileChooser;
+    private File file;
     private PopOutWindow popOutWindow = new PopOutWindow();
     private static String language = App.getLanguage();
 
@@ -55,5 +67,22 @@ public class PrimaryController {
             popOutWindow.messageBox(bundle.getString("error_title"),
                     bundle.getString("error_language_choice"), Alert.AlertType.WARNING);
         }
+    }
+
+    @FXML
+    public void onActionButtonLoad(ActionEvent actionEvent) throws IOException {
+        SudokuBoard sudokuBoard;
+        fileChooser = new FileChooser();
+        file = fileChooser.showOpenDialog(thisStage);
+        SudokuBoardDaoFactory factory = new StreamSudokuBoardFactory();
+        Dao<SudokuBoard> sudokuBoardDaoFile;
+        sudokuBoardDaoFile = factory.getFileDao(file.getAbsolutePath());
+        sudokuBoard=sudokuBoardDaoFile.read();
+        Locale.setDefault(new Locale(language));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.languages");
+        LoadedController loaderController = new LoadedController(sudokuBoard);
+        loaderController.showStage();
+
+
     }
 }
