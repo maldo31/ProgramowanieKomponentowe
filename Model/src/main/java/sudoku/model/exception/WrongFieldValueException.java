61f3a6d51f41ac5acfd7c56996b637fff8ad2d4e
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-01-22 11:47:26 +0100

    Custom exceptions added

---
+++ Model/src/main/java/sudoku/model/exception/WrongFieldValueException.java
@@ -0,0 +1,7 @@
+package sudoku.model.exception;
+
+public class WrongFieldValueException extends SudokuBoardException{
+    public WrongFieldValueException(){
+        super("Field value must be 1-9 inclusive");
+    }
+}
