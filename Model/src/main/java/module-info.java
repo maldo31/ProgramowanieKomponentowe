e6ca9aa751ce2727837b70689d4557bbaf68d669
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-01-25 02:05:27 +0100

    "Wstępna konfiguracja bazy"

--- Model/src/main/java/module-info.java
+++ Model/src/main/java/module-info.java
@@ -1,8 +1,15 @@
+/**
+ *
+ */
 module ModelProject {
     requires org.apache.commons.lang3;
     requires java.desktop;
     requires javafx.base;
     requires org.slf4j;
+    requires hibernate.entitymanager;
+    requires org.hibernate.commons.annotations;
+    requires java.persistence;
+
     opens sudoku.model;
     exports sudoku.model;
     exports sudoku.model.exception;
